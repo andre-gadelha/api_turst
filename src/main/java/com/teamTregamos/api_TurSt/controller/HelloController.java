dd32d3bdbdef8fa6029d573fc6a828d996168ff1
andre-gadelha <adr@gmail.com>
2025-10-23 16:44:09 -0300

    Ahustando propriedades Name e Version para a API

--- src/main/java/com/teamTregamos/api_TurSt/controller/HelloController.java
+++ src/main/java/com/teamTregamos/api_TurSt/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.teamTregamos.api_TurSt.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +10,20 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequestMapping("/")
 public class HelloController {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @GetMapping
     public String hello(UriComponentsBuilder uriBuilder){
+
         //Recuperando a URL
         var uri = uriBuilder.path("/swagger-ui/index.html").buildAndExpand().toUri();
 
-        return "Olá!\n Este é o diretório raiz da API - TusSt. " +
-                "Leia a documentação: " +
-                "<a href='" + uri + "'>Documentação</a>";
+        return "<h3>Olá!<br><br>" +
+                "Este é o diretório raiz da API - " + appName + " Versão: 0.0.4  .<br><br>" +
+                //"Este é o diretório raiz da API - .<br><br>" +
+                "Acesse os Endpoints: " +
+                "<a href='" + uri + "'>Documentação</a></h3>";
     }
 
 }
